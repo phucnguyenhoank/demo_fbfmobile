644fa6ad0156ae3e5b1b8826ac83d661980753e5
HoangPhuc <phucnguyeho@gmail.com>
2025-05-12 10:24:29 +0700

    add bottom navigation

--- app/src/main/java/com/example/demo_fbfmobile/MainActivity.java
+++ app/src/main/java/com/example/demo_fbfmobile/MainActivity.java
@@ -49,37 +49,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
-        Button eatButton = findViewById(R.id.btnEat);
-        eatButton.setOnClickListener(v -> {
-            TokenManager tokenManager = new TokenManager(MainActivity.this);
-            if (tokenManager.getToken() != null && !tokenManager.isTokenExpired()) {
-                // Token valid, go to HomeActivity
-                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
-                startActivity(intent);
-            } else {
-                // Token missing or expired, go to LoginActivity
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(intent);
-            }
-        });
 
-        Button registerButton = findViewById(R.id.btnRegister);
-        registerButton.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
-            startActivity(intent);
-        });
-
-        Button btnResetPassword = findViewById(R.id.btnResetPassword);
-        btnResetPassword.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, ResetPasswordActivity.class);
-            startActivity(intent);
-        });
+//        Button eatButton = findViewById(R.id.btnEat);
+//        eatButton.setOnClickListener(v -> {
+//            TokenManager tokenManager = new TokenManager(MainActivity.this);
+//            if (tokenManager.getToken() != null && !tokenManager.isTokenExpired()) {
+//                // Token valid, go to HomeActivity
+//                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+//                startActivity(intent);
+//            } else {
+//                // Token missing or expired, go to LoginActivity
+//                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+//
+//        Button registerButton = findViewById(R.id.btnRegister);
+//        registerButton.setOnClickListener(v -> {
+//            Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+//            startActivity(intent);
+//        });
+//
+//        Button btnResetPassword = findViewById(R.id.btnResetPassword);
+//        btnResetPassword.setOnClickListener(v -> {
+//            Intent intent = new Intent(MainActivity.this, ResetPasswordActivity.class);
+//            startActivity(intent);
+//        });
 
         rvFoods = findViewById(R.id.rvFoods);
         adapter = new FoodAdapter();
